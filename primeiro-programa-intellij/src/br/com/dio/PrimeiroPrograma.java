95a9a1e6021f3508475504f5d0f975b37216a710
Noriak <ruan-rrc@hotmail.com>
2023-03-09 16:11:46 -0300

    Changing only to test intellij integration

--- primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,11 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato("Catito", "Roxo", 29);
         System.out.println(gato);
-        /*int a = 5;
+        int a = 5;
         int b = 3;
 
-        System.out.println("Hello mundo! " + (a+b));*/
+        System.out.println("Hello mundo! " + (a+b));
+
 
 
     }
